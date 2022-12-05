384927cec4cb045590054dcf7f755ee9bc4298e1
mikemilly <millymike599@gmail.com>
2022-12-05 22:57:27 +0100

    refactored

--- Blog.Api/Dtos/RegisterAuthorRequestDto.cs
+++ Blog.Api/Dtos/RegisterAuthorRequestDto.cs
@@ -4,10 +4,10 @@ namespace Blog.Api.Dtos;
 
 public class RegisterAuthorRequestDto
 {
-    [Required] public string EmailAddress { get; set; }
-    public string FirstName { get; set; }
+    [Required] public string EmailAddress { get; set; } 
+    public string FirstName { get; set; } 
     public string? LastName { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-    [Required] public string Description { get; set; }
+    public string Username { get; set; } 
+    public string Password { get; set; } 
+    [Required] public string Description { get; set; } 
 }
--- Blog.Api/Dtos/Validators/RegisterAuthorRequestDtoValidator.cs
+++ Blog.Api/Dtos/Validators/RegisterAuthorRequestDtoValidator.cs
@@ -10,6 +10,6 @@ public class RegisterAuthorRequestDtoValidator : AbstractValidator<RegisterAutho
         RuleFor(x => x.FirstName).NotEmpty().WithMessage("Must have First Name");
         RuleFor(x => x.Username).NotEmpty().MinimumLength(4).WithMessage("Username must be at least 4 characters");
         RuleFor(x => x.Password).MinimumLength(8).WithMessage("Password must be at least 8 character");
-        RuleFor(x => x.Description).MinimumLength(250);
+        RuleFor(x => x.Description).MinimumLength(50);
     }
 }
